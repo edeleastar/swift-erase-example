f0a6de8631d0dd9fa07defe5c57cb804ab41c454
Eamonn de Leastar <edeleastar@gmail.com>
2014-06-10 08:16:24 +0100

    use closure for filtering

--- erase/main.swift
+++ erase/main.swift
@@ -5,24 +5,11 @@ class Erase
   func main()
   {
     var names = ["ted", "fred", "jed", "ned"]
-    var short_names = filterLongerThan(names, length:3)
+    var short_names = names.filter { countElements($0) < 4 }
     for name in short_names
     {
       println (name)
     }
-  }
-  
-  func filterLongerThan (strings : String[], length : Int) -> String[]
-  {
-    var result = String[]()
-    for s in strings
-    {
-      if countElements(s) < length + 1
-      {
-        result.append(s)
-      }
-    }
-    return result
   }
 }
 
