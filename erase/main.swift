9dba3a7a09610ef2a6340d24e23a5d43addbb2c9
Eamonn de Leastar <edeleastar@gmail.com>
2014-06-10 08:07:52 +0100

    Colllection literals

--- erase/main.swift
+++ erase/main.swift
@@ -4,12 +4,7 @@ class Erase
 {
   func main()
   {
-    var names = String[]()
-    names.append ("ted")
-    names.append ("fred")
-    names.append ("jed")
-    names.append ("ned")
-    println(names)
+    var names = ["ted", "fred", "jed", "ned"]
     var short_names = filterLongerThan(names, length:3)
     for name in short_names
     {
