bde6cd51754b052ae264d97d12ce6e447ea7ef67
Eamonn de Leastar <edeleastar@gmail.com>
2014-06-10 07:31:28 +0100

    initial verbose version

--- erase/main.swift
+++ erase/main.swift
@@ -1,12 +1,37 @@
-//
-//  main.swift
-//  erase
-//
-//  Created by edeleastar on 10/06/2014.
-//  Copyright (c) 2014 WIT. All rights reserved.
-//
-
 import Foundation
 
-println("Hello, World!")
+class Erase
+{
+  func main()
+  {
+    var names = String[]()
+    names.append ("ted")
+    names.append ("fred")
+    names.append ("jed")
+    names.append ("ned")
+    println(names)
+    var short_names = filterLongerThan(names, length:3)
+    for name in short_names
+    {
+      println (name)
+    }
+  }
+  
+  func filterLongerThan (strings : String[], length : Int) -> String[]
+  {
+    var result = String[]()
+    for s in strings
+    {
+      if countElements(s) < length + 1
+      {
+        result.append(s)
+      }
+    }
+    return result
+  }
+}
+
+var erase = Erase()
+erase.main()
+
 
